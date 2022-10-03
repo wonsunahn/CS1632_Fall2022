4146a9dd510309233743d780d69a5ecb11da9414
wonsunahn <wahn@pitt.edu>
2022-10-03 05:54:03 -0400

    Added separate integration and unit tests.

--- study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/DeathStarTest.java
+++ study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/DeathStarTest.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 
 public class DeathStarTest {
 
-	/*
+	/**
+	 * Write an integration test for shoot.
+	 * 
 	 * Preconditions: A new DeathStar deathStar is created
 	 *                A new Planet planet is created with hit points 10
 	 * Execution Steps: shoot planet with deathStar
@@ -15,7 +17,21 @@ public class DeathStarTest {
 	 *                 Planet receives a damage of 100 hit points
 	 */
 	@Test
-	public void testShootPlanet() {
+	public void testShootPlanetIntegration() {
+		// TODO: Fill in!
+	}
+
+	/**
+	 * Write an unit test for shoot.
+	 * 
+	 * Preconditions: A new DeathStar deathStar is created
+	 *                A new Planet planet is created with hit points 10
+	 * Execution Steps: shoot planet with deathStar
+	 * PostConditions: Return value of deathStar.shoot(planet) is "Wimpy planet was hit by the superlaser!"
+	 *                 Planet receives a damage of 100 hit points
+	 */
+	@Test
+	public void testShootPlanetUnit() {
 		// TODO: Fill in!
 	}
 }
--- study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/SquareTest.java
+++ study_guides/midterm_1_practice/src/test/java/edu/pitt/cs/SquareTest.java
@@ -7,14 +7,29 @@ import org.junit.Test;
 
 public class SquareTest {
 
-	/*
+	/**
+	 * Write an integration test for setSquared.
+	 * 
 	 * Preconditions: A new Square square is created
 	 *                A new Number number is created
 	 * Execution Steps: Call square.setSquared(number, 3)
 	 * PostConditions: The value of number is set to 9
 	 */
 	@Test
-	public void testSetSquared() {
+	public void testSetSquaredIntegration() {
+		// TODO: Fill in!
+	}
+
+	/**
+	 * Write a unit test for setSquared.
+	 * 
+	 * Preconditions: A new Square square is created
+	 *                A new Number number is created
+	 * Execution Steps: Call square.setSquared(number, 3)
+	 * PostConditions: The value of number is set to 9
+	 */
+	@Test
+	public void testSetSquaredUnit() {
 		// TODO: Fill in!
 	}
 
